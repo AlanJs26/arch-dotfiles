dd3bc27f9fbf5c3cf08d40be94b802941abaa6ff
AlanJS26 <alanjoses.29@gmail.com>
2023-11-21 20:23:42 -0300

    r9xj3iygffiiRdo7 21-11-2023

--- archdots/generate-archdots.sh
+++ archdots/generate-archdots.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 (cd ~/.local/share/chezmoi/archdots && bashly generate --upgrade)
-chmod +x ~/.local/share/chezmoi/archdots/src/**/*.sh
+chmod +x $HOME/.local/share/chezmoi/archdots/src/**/*.sh
